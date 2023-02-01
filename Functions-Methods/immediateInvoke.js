03149bd0711882bb16595bb983671fb2138f47d3
Davinsicode <sureshroshan2@gmail.com>
2023-02-01 12:50:05 +0530

    "01-02-2023 Script Executions"

--- Functions-Methods/immediateInvoke.js
+++ Functions-Methods/immediateInvoke.js
@@ -1,5 +1,7 @@
 // Here we no need to invoke the Function
-(function studentData(Name, Percent) 
+console.log("hello");
+
+(function (Name, Percent) 
 {
     console.log(Name+" got: "+Percent+"%")    
 })("Roshan",85)
